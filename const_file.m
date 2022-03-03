b973e113834726dff35f6afda19812cf3190320c
mb14g15 <mb14g15@JFB25-R1011-W13.soton.ac.uk>
2022-03-03 16:04:34 +0000

    update parameters, conv electrolyser ssc params to variables

--- const_file.m
+++ const_file.m
@@ -1,24 +1,26 @@
 %%%% Place all constants here
-load('const.mat');
+%load('const.mat');
 
 %%
-R	   = 8.314;    % Universal gas const [J/(mol*K)]
-F	   = 96485.3;  % Faraday  [C/mol]
-HHV_h2 = 1.419E8;  % H2 higher heating value [J/kg]
+const.R	     = 8.314;    % Universal gas const [J/(mol*K)]
+const.F	     = 96485.3;  % Faraday  [C/mol]
+const.HHV_h2 = 1.419E8;  % H2 higher heating value [J/kg]
 
 % Density [kg/m^3]
-rho_h2  = 0.082;   % H2
-rho_o2  = 1.31;    % O2
-rho_h2o = 1000;    % Water
+const.rho_h2  = 0.082;   % H2
+const.rho_o2  = 1.31;    % O2
+const.rho_h2o = 1000;    % Water
 
 % Molar mass [kg/mol]
-M_h2  = 0.0020158;  % H2
-M_o2  = 0.0319988;  % O2
-M_h2o = 0.0180152;  % Water
+const.M_h2  = 0.0020158;  % H2
+const.M_o2  = 0.0319988;  % O2
+const.M_h2o = 0.0180152;  % Water
 
 % Sp. gas  [J/(kg*K)]
-R_h2  = round(R/M_h2,  5, 'significant');  % H2
-R_o2  = round(R/M_o2,  5, 'significant');  % O2
-R_h2o = round(R/M_h2o, 5, 'significant');  % Water
+const.R_h2  = round(const.R/const.M_h2,  5, 'significant');  % H2
+const.R_o2  = round(const.R/const.M_o2,  5, 'significant');  % O2
+const.R_h2o = round(const.R/const.M_h2o, 5, 'significant');  % Water
 
 % Membrane params
+const.Dw_mem  = 1.28E-10;  % Water diffusion coeff. @ Nafion membrane []
+const.K_darcy = 1.58E-18;  % Nafion membrane permeability to water [m^2]
--- parameters.m
+++ parameters.m
@@ -1,31 +1,48 @@
 %% PLACE INPUT PARAMETERS IN THIS FILE %%
 
-%%% Constants
+%%% Load constants and gas properties
 const_file;
-
-%%% Gas properties
 data_gas_file;
 
+%% Ambient conditions
+amb.T     = 20 + 273.15;       % Ambient temperature  [degC -> K]
+amb.p     = 1  * 101325;       % Ambient air pressure [atm -> Pa]
+amb.phi   = 0.60;              % Ambient humidity     [0-1]
 
-% Ambient conditions
-T_amb     = 20;       % Ambient temperature [deg C]
-p_amb     = 1;        % Ambient air pressure [atm]
-phi_amb   = 0.60;     % Ambient humidity [0-1]
+%% Control inputs
+control.T_stk  = 80 + 273.15;  % Nominal operating temperature [deg C]  (Default: 80)
+control.dT_stk = 5;            % Coolant T-diff thru stack     [C or K] (Default: 5)
 
-% Stack operating conditions
-T_stk     = 80;       % Nominal operating temperature [deg C]			  (Default: 80)
-dT_stk    = 5;        % Coolant T-diff between outlet & inlet [C or K]  (Default: 5)
 
-% Stack specifications
-A_cel     = 200;      % Cell active area [cm^2]
-M_mem     = 1.1;      % Membrane dry molar mass [kg/mol]
-rho_mem   = 1.98E-3;  % Membrane dry density [kg/cm^3]
-L_mem     = 0.01;     % Membrane thickness [cm]
-D_cl_t    = 2;	      % Cooling tube diameter [mm]
-N_t_bp    = 50;       % No. cooling tubes per bipolar plate []
+%% Stack specifications
+pemel.N_cel     = 30;          % Number of cells in stack []
+pemel.A_cel     = 0.1;         % Cell active area         [m^2]
 
-% BoP efficiencies
-eff_cmp  = 0.90;      % Compressor efficiency []
-eff_rcrc = 0.90;	  % Recirculator efficiency []
-eff_pmp  = 0.90;      % Coolant pump efficiency []
-eff_fan  = 0.90;      % Fan efficiency []
+%pemel.Ac_an  = 2.5E-4;         % Anode channel cross-section area      [m^2]
+%pemel.Prm_an = 0.002;          % Anode channel cross-section perimeter [m]
+%pemel.L_an   = 0.01;           % Anode channel length                  [m]
+pemel.vol_an = 0.1;            % Cathode channel volume                [m^3]
+
+%pemel.Ac_ca  = 2.5E-4;         % Cathode channel cross-section area      [m^2]
+%pemel.Prm_ca = 0.002;          % Cathode channel cross-section perimeter [m]
+%pemel.L_ca   = 0.01;           % Cathode channel length                  [m]
+pemel.vol_ca = 0.1;            % Cathode channel volume                  [m^3]
+
+pemel.A_mem   = pemel.A_cel;   % Membrane area           [m^2]
+pemel.thk_mem = 1E-4;          % Membrane thickness      [m]
+%pemel.M_mem   = 1.1;           % Membrane dry molar mass [kg/mol]
+%pemel.rho     = 1.98E-3;       % Membrane dry density    [kg/cm^3]
+
+pemel.N_clt   = 50;             % No. cooling tubes per BP plate      []
+pemel.Ac_clt  = 1E-6;          % Cooling tube cross-section area      [m^2]
+pemel.Prm_clt = 4E-3;          % Cooling tube cross-section perimeter [m]
+pemel.L_clt   = 0.5;           % Cooling tube length                  [m]
+
+pemel.thk_bp  = 0.002;         % BP plate thickness                   [m]
+pemel.cp_bp   = 100;           % BP plate Sp. heat (Cp)               [J/(kg*K)]
+pemel.k_bp    = 1;             % BP plate heat conductivity           [W/(m*K)]
+
+
+%% BoP efficiencies
+BoP.eff_pmp  = 0.90;  % Coolant pump efficiency []
+BoP.eff_fan  = 0.90;  % Fan efficiency []
