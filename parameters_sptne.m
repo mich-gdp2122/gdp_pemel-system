3df7e1eacd438c202afa29b084be6cd9926f6e44
mb14g15-soton <mb14g15@soton.ac.uk>
2022-04-26 21:17:32 +0100

    add serpentine configs

--- parameters_sptne.m
+++ parameters_sptne.m
@@ -18,20 +18,19 @@ pemel.A_cel   = 0.1*100^2;   % Cell active area   [cm^2]
 pemel.eff_stk = 0.79;		 % Stack efficiency   []
 
 % Cooling channels
-clch.N   = 2*50;				   % No. cooling tubes per BP plate       []
-clch.L   = 0.25;				   % Cooling tube length                  [m]
-clch.W   = 0.001;				   % Cooling tube width					  [m]
-clch.H   = 0.0005;				   % Cooling tube height				  [m]
-clch.Ac  = clch.W*clch.H;		   % Cooling tube cross-section area      [m^2]
-clch.Prm = 2*(clch.W+clch.H);      % Cooling tube cross-section perimeter [m]
+clch.N   = 2*60;            % No. cooling tubes per BP plate       []
+clch.L   = 0.25;          % Cooling tube length                  [m]
+clch.W   = 0.001;		  % Cooling tube width					 [m]
+clch.Ac  = clch.W^2;      % Cooling tube cross-section area      [m^2]
+clch.Prm = 4*clch.W;      % Cooling tube cross-section perimeter [m]
 
 % Bipolar plate
-% bp.L   = 0.5;			% BP plate length [m]
-% bp.W   = 0.2;			% BP plate width [m]
-% bp.thk = 0.002;			% BP plate thickness [m]
-% bp.rho = 4494;			% BP density [kg/m^3]
-% bp.cp  = 534;			% BP plate Sp. heat (Cp)               [J/(kg*K)]
-% bp.k   = 20.2;			% BP plate heat conductivity           [W/(m*K)]
+bp.L   = 0.5;			% BP plate length [m]
+bp.W   = 0.2;			% BP plate width [m]
+bp.thk = 0.002;			% BP plate thickness [m]
+bp.rho = 4494;			% BP density [kg/m^3]
+bp.cp  = 534;			% BP plate Sp. heat (Cp)               [J/(kg*K)]
+bp.k   = 20.2;			% BP plate heat conductivity           [W/(m*K)]
 
 % Process channels
 prch.N   = 4;			% No. anode/cathode channels each per cell []
@@ -60,11 +59,11 @@ h2o.dp_stk    = 5;			% Stack pressure drop [bar]
 %h2o.mdot_stk  = 12;		% Nominal mass flow rate per stack [kg/s]
 
 % Coolant
-clnt.Vldot_ch  = 0.33*1e-6;					    % Volume flow rate per channel [mL/s -> m^3/s]
-clnt.T_stk_in  = 341.44;						% Coolant inlet temperature @ stack [K] 
-clnt.T_stk_out = 347.32;						% Coolant outlet temperature [K]
-%clnt.p_stk_in  = 2742.5 + amb.p;			    % Coolant stack inlet pressure [Pa]
-clnt.dp_stk    = 14209;						    % Coolant stack pressure drop [Pa]
+clnt.Vldot_ch  = 0.25*1e-6;					   % Volume flow rate per channel [mL/s -> m^3/s]
+clnt.T_stk_in  = 339;						   % Coolant inlet temperature @ stack [K] 
+clnt.T_stk_out = clnt.T_stk_in + 6;			   % Coolant outlet temperature [K]
+%clnt.p_stk_in  = 2742.5 + amb.p;			   % Coolant stack inlet pressure [Pa]
+clnt.dp_stk    = 2600;						   % Coolant stack pressure drop [Pa]
 %clnt.p_stk_out = clnt.p_stk_in - clnt.dp_stk;  % Coolant outlet pressure [Pa]
 
 
