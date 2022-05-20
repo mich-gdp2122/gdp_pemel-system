670e6d4e72b019af4f23f8e9141a3ff2d0d237be
mb14g15-soton <mb14g15@soton.ac.uk>
2022-05-20 14:28:19 +0100

    More cleanup

--- _OPEN_PROJECT_Win10.bat
+++ _OPEN_PROJECT_Win10.bat
@@ -4,8 +4,12 @@
 IF NOT EXIST "%USERPROFILE%\.ssh" (
 	MD "%USERPROFILE%\.ssh"
 )
-COPY ".ssh\id_rsa" "%USERPROFILE%\.ssh\id_rsa"
-COPY ".ssh\id_rsa.pub" "%USERPROFILE%\.ssh\id_rsa.pub"
+IF EXIST ".ssh\id_rsa" (
+	COPY ".ssh\id_rsa" "%USERPROFILE%\.ssh\id_rsa"
+)
+IF EXIST ".ssh\id_rsa.pub" (
+	COPY ".ssh\id_rsa.pub" "%USERPROFILE%\.ssh\id_rsa.pub"
+)
 
 :: start matlab project
 matlab.exe -r "setenv('HOME', [getenv('USERPROFILE')]); open('GDPPEMELSystem.prj')"
