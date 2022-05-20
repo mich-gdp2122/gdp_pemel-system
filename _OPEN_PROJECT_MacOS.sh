5119d5558b7c21ab1be016f3d905eba9324157a4
mb14g15-soton <mb14g15@soton.ac.uk>
2022-05-20 16:53:39 +0100

    Final cleanup

--- _OPEN_PROJECT_MacOS.sh
+++ _OPEN_PROJECT_MacOS.sh
@@ -2,13 +2,6 @@
 
 HERE="${0:h}"
 
-# copy repo ssh keys to working dir
-if [[ ! -e ~/".ssh/" ]]; then
-    mkdir ~/".ssh/"
-fi
-cp -f "$HERE/.ssh/id_rsa" ~/".ssh/id_rsa"
-cp -f "$HERE/.ssh/id_rsa.pub" ~/".ssh/id_rsa.pub"
-
 # open matlab project
 /Applications/MATLAB_R2022a.app/bin/matlab -r "open ('$HERE/GDPPEMELSystem.prj')" ; disown
 
--- _OPEN_PROJECT_Win10.bat
+++ _OPEN_PROJECT_Win10.bat
@@ -1,15 +1,4 @@
 @echo off
 
-:: copy repo ssh keys to working dir
-IF NOT EXIST "%USERPROFILE%\.ssh" (
-	MD "%USERPROFILE%\.ssh"
-)
-IF EXIST ".ssh\id_rsa" (
-	COPY ".ssh\id_rsa" "%USERPROFILE%\.ssh\id_rsa"
-)
-IF EXIST ".ssh\id_rsa.pub" (
-	COPY ".ssh\id_rsa.pub" "%USERPROFILE%\.ssh\id_rsa.pub"
-)
-
 :: start matlab project
 matlab.exe -r "setenv('HOME', [getenv('USERPROFILE')]); open('GDPPEMELSystem.prj')"
